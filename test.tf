6bb12872980a5db87a4d9678b96879d328cb976d
Jeevan Joseph <jeevan.g@gmail.com>
2019-02-08 17:03:03 -0800

    initial commit

---
+++ test.tf
@@ -0,0 +1,18 @@
+variable "region" {
+  default = "us-phoenix-1"
+  description = "The region to target with this provider configuration."
+}
+provider "oci" {
+  auth = "InstancePrincipal"
+  region = "${var.region}"
+}
+
+# Get a list of Availability Domains
+data "oci_identity_availability_domains" "ads" {
+  compartment_id = "ocid1.tenancy.oc1..aaaaaaaawpqblfemtluwxipipubxhioptheej2r32gvf7em7iftkr3vd2r3a"
+}
+
+# Output the result
+output "show-ads" {
+  value = "${data.oci_identity_availability_domains.ads.availability_domains}"
+}
